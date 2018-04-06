a691991619cd0d35875284aef9bd3e07faeae4bf
joseph.cardillo <joseph.s.cardillo@gmail.com>
2018-04-06 13:02:07 -0400

    updated feed to show user

--- app.rb
+++ app.rb
@@ -2,8 +2,15 @@ require 'sinatra'
 require 'sinatra/activerecord'
 require 'bundler/setup'
 require './models'
+enable :sessions
 
 set :database, "sqlite3:microblog.sqlite3"
+
+def current_user
+    if session[:user_id]
+      @current_user = User.find(session[:user_id])
+    end
+end
 
 
 get '/' do
@@ -12,37 +19,42 @@ get '/' do
 end
 
 get '/profile' do
-  # @users = User.all
+  # @user = User.all
   erb :profile
 end
 
-# get '/user/:id' do
-#   @user = User.find(params[:id])
-#   @posts = @user.posts
-#   erb :user
-# end
+get '/feed' do
+  @posts = Post.all
+  erb :feed
+end
 
-# post '/users/new' do
-#   puts "*********************"
-#   puts params.inspect
-#   puts "*********************"
-#   # should be called user instead of post
-#   User.create(params[:post])
-#   redirect '/users'
-# end
-#
-# post '/sign-in' do
-#   @user = User.where(fname: params[:fname]).first
-#   if @user.password == params[:password]
-#     session[:user_id] = @user.id
-#     flash[:notice] = "You've been signed in successfully."
-#     redirect '/'
-#   else
-#     flash[:notice] = "There was a problem signing you in."
-#     redirect '/sign-in-failed'
-#   end
-# end
-#
-# get '/sign-in-failed' do
-#   erb :sign_in_failed
-# end
+get '/user/:id' do
+  @user = User.find(params[:id])
+  @posts = @user.posts
+  erb :user
+end
+
+post '/users/new' do
+  puts "*********************"
+  puts params.inspect
+  puts "*********************"
+  # should be called user instead of post
+  User.create(params[:post])
+  redirect '/users'
+end
+
+post '/sign-in' do
+  @user = User.where(fname: params[:fname]).first
+  if @user.password == params[:password]
+    session[:user_id] = @user.id
+    flash[:notice] = "You've been signed in successfully."
+    redirect '/'
+  else
+    flash[:notice] = "There was a problem signing you in."
+    redirect '/sign-in-failed'
+  end
+end
+
+get '/sign-in-failed' do
+  erb :sign_in_failed
+end
---
+++ db/seeds.rb
@@ -0,0 +1,32 @@
+# User.create(fname: "Luke", lname: "Skywalker", password: "ls", email: 22, username: )
+# User.create(fname: "John", lname: "Smith", password: "ls", age: 22)
+# User.create(fname: "Luke", lname: "Skywalker", password: "ls", age: 22)
+# User.create(fname: "Luke", lname: "Skywalker", password: "ls", age: 22)
+# User.create(fname: "Luke", lname: "Skywalker", password: "ls", age: 22)
+
+# or ...
+
+user_list = [
+  ["Joseph", "Cardillo", "123", "joseph@email.com", "jcardillo"],
+  ["Peter", "Pumpkin", "234", "peter@email.com", "ppumpkin"],
+  ["Beverly", "Johnson", "beverly@email.com", "bjohnson"],
+  ["Carly", "Closs", "456", "carly@email.com", "ccloss"]
+]
+
+user_list.each do |fname, lname, password, email, username|
+  User.create(fname: fname, lname: lname, password: password, email: email, username: username)
+end
+
+post_list = [
+  ["Yesterday Was Cold", "It was really, really cold.", 1],
+  ["Tomorrow Will Be Hot", "It will be really, really hot.", 2],
+  ["In Two Days", "I will ride my bike.", 3],
+  ["On Thursday", "It will snow.", 4]
+]
+
+post_list.each do |title, content, user_id|
+  Post.create(title: title, content: content, user_id: user_id)
+end
+
+# or...
+# Post.create(title: "Why I love lemons", content: "They are great", user_id: 1)
