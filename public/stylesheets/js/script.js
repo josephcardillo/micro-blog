0d35270f181e04d840b674fd2b35369ea4d96a9a
joseph.cardillo <joseph.s.cardillo@gmail.com>
2018-04-10 15:45:42 -0400

    made unicorn flip and move across the page

--- public/stylesheets/js/script.js
+++ public/stylesheets/js/script.js
@@ -5,7 +5,27 @@ var toggle = {
   close() {
     document.getElementsByClassName("toggleEditUser")[0].style.display = "none";
   },
-  img1() {
-    $(".uniImage1").fadeTo("slow", 1);
+  uniAni() {
+    $("#clickMe").click(function() {
+      $(".uniImage2").animate({
+        width: "300px",
+        position: "fixed",
+        top: "-200px",
+        left: "-75px"
+      }, 3000)
+    });
   }
 }
+
+$("#clickMe").rotate({
+  bind:
+  {
+    click: function() {
+    $(this).rotate({
+      duration: 3000,
+      angle: 0,
+      animateTo:360
+      })
+    },
+  }
+});
